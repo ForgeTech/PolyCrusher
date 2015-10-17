b3d9e01d29d1b9ff73b8bc53aae3c971875c859b
Julia Angerer <jul.ya@gmx.at>
2015-10-17 17:40:04 +0200

    Added: basic trap class

--- Assets/Scripts/Traps/DeathTrap.cs
+++ Assets/Scripts/Traps/DeathTrap.cs
@@ -1,22 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DeathTrap : MonoBehaviour,ITriggerable {
-
-    #region Class Members
-    //how long the trap needs to reset
-    [SerializeField]
-    public float trapActiveTime = 0.5f;
-
-    //trap can only be triggered if this is false
-    protected bool isActive = false;
-
-    #endregion
+public class DeathTrap : Trap,ITriggerable {
 
     #region Class Methods
-
-    //trigger method that manages kill & trap animation
-    public void Trigger(Collider other) {
+    
+    public override void Trigger(Collider other) {
         if (isActive == false)
         {
             isActive = true;
@@ -37,19 +26,6 @@ public class DeathTrap : MonoBehaviour,ITriggerable {
                 enemy.InstantKill();
             }
         }
-    }
-
-    //calls the trigger method if player/enemy collides with the trap
-    void OnTriggerEnter(Collider other)
-    {
-        Trigger(other);
-    }
-
-    //keeps the trap from triggering too often
-    IEnumerator WaitForActive()
-    {
-        yield return new WaitForSeconds(trapActiveTime);
-        isActive = false;
     }
 
     #endregion
---
+++ Assets/Scripts/Traps/Trap.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Trap : MonoBehaviour,ITriggerable {
+
+    #region Class Members
+    //how long the trap needs to reset
+    [SerializeField]
+    protected float trapActiveTime = 0.5f;
+
+    //trap can only be triggered if this is false
+    protected bool isActive = false;
+
+    #endregion
+
+    #region Class Methods
+
+    //trigger method that manages kill & trap animation
+    public virtual void Trigger(Collider other){}
+
+    //calls the trigger method if player/enemy collides with the trap
+    void OnTriggerEnter(Collider other)
+    {
+        Trigger(other);
+    }
+
+    //keeps the trap from triggering too often
+    protected virtual IEnumerator WaitForActive()
+    {
+        yield return new WaitForSeconds(trapActiveTime);
+        ResetTrap();
+    }
+
+    //resets trap
+    protected virtual void ResetTrap()
+    {
+        isActive = false;
+    }
+
+    #endregion
+}
