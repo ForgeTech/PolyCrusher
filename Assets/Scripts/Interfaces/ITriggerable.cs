c3d394b3b478860d90a7b58b16b6ff3209d871ee
July <jul.ya@gmx.at>
2015-10-17 17:01:25 +0200

    Added: trigger interface, death trap script with simple death, bear trap model + animation + material

---
+++ Assets/Scripts/Interfaces/ITriggerable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Interface for objects which use a trigger (such as traps, barriers, ...).
+/// </summary>
+public interface ITriggerable
+{
+
+    /// <summary>
+    /// Handles the object when triggered by a player/enemy.
+    /// </summary>
+    void Trigger(Collider other);
+}
---
+++ Assets/Scripts/Traps/DeathTrap.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrap : MonoBehaviour,ITriggerable {
+
+    #region Class Members
+    //how long the trap needs to reset
+    [SerializeField]
+    public float trapActiveTime = 0.5f;
+
+    //trap can only be triggered if this is false
+    protected bool isActive = false;
+
+    #endregion
+
+    #region Class Methods
+
+    //trigger method that manages kill & trap animation
+    public void Trigger(Collider other) {
+        if (isActive == false)
+        {
+            isActive = true;
+            StartCoroutine(WaitForActive());
+            GetComponent<Animation>().Play("strike");
+
+            if (other.tag == "Player")
+            {
+                // get the BasePlayer of the Game Object
+                BasePlayer player = other.GetComponent<BasePlayer>();
+                player.InstantKill();
+            }
+
+            if (other.tag == "Enemy")
+            {
+                // get the BaseEnemy of the Game Object
+                BaseEnemy enemy = other.GetComponent<BaseEnemy>();
+                enemy.InstantKill();
+            }
+        }
+    }
+
+    //calls the trigger method if player/enemy collides with the trap
+    void OnTriggerEnter(Collider other)
+    {
+        Trigger(other);
+    }
+
+    //keeps the trap from triggering too often
+    IEnumerator WaitForActive()
+    {
+        yield return new WaitForSeconds(trapActiveTime);
+        isActive = false;
+    }
+
+    #endregion
+}
