04d7958da66ca2d1bae7dfec54e4345574b5dcda
Parzival Legmaier <legmaier@gmx.at>
2015-11-02 18:31:29 +0100

    Added:  - Sign script (Sign tweening)
    Updated:
     - Sign prefab size

---
+++ Assets/Scripts/PowerUps/SignScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handles the resizing of the signs.
+/// </summary>
+public class SignScript : MonoBehaviour
+{
+    [Tooltip("The time for the scaling.")]
+    [SerializeField]
+    protected float scaleTime = 3f;
+
+    Vector3 originalSize;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalSize = transform.localScale;
+        InvokeRepeating("MakeScaleTransition", 0f, scaleTime + 0.05f);
+	}
+
+    private void MakeScaleTransition()
+    {
+        transform.localScale = Vector3.zero;
+        StartCoroutine(transform.ScaleTo(new Vector3(originalSize.x, originalSize.y, originalSize.z), scaleTime, AnimCurveContainer.AnimCurve.shortUpscale.Evaluate));
+    }
+}
