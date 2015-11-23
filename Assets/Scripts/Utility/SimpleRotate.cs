96b74e81b8e9a8c8fc0515862c55684e07b225f5
Parzival Legmaier <legmaier@gmx.at>
2015-11-23 14:27:51 +0100

    Changed the rotation behaviour of the tetris bullets

--- Assets/Scripts/Utility/SimpleRotate.cs
+++ Assets/Scripts/Utility/SimpleRotate.cs
@@ -4,10 +4,30 @@ using System.Collections;
 public class SimpleRotate : MonoBehaviour
 {
     public Vector3 rotation = Vector3.zero;
-	
+
+    [SerializeField]
+    [Tooltip("Activates a time based rotation.")]
+    protected bool timeBasedRotation = false;
+
+    [SerializeField]
+    protected float rotateInterval = 0.3f;
+
+    protected float currentInterval = 0f;
+
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Rotate(rotation);
+        if (!timeBasedRotation)
+            transform.Rotate(rotation);
+        else
+        {
+            if (currentInterval >= rotateInterval)
+            {
+                transform.Rotate(rotation);
+                currentInterval = 0;
+            }
+
+            currentInterval += Time.deltaTime;
+        }
 	}
 }
