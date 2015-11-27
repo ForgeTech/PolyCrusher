c2daf523ace1f038d7bea88f4784c2f6d34475a7
Parzival Legmaier <legmaier@gmx.at>
2015-11-27 14:32:05 +0100

    Added a new Song (Crush 'em all)
    Added a menu item for the Transform to reset it easily.

---
+++ Assets/Editor/TransformResetMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TransformResetMenuItem
+{
+    [MenuItem("CONTEXT/Transform/ResetValues")]
+    private static void ResetValues(MenuCommand command)
+    {
+        Transform t = command.context as Transform;
+
+        t.position = Vector3.zero;
+        t.localScale = new Vector3(1, 1, 1);
+        t.rotation = Quaternion.identity;
+    }
+}
