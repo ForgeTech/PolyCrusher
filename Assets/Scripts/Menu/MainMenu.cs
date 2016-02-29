35cb2bfbf6afa428117e774afc47c8a120a1cc60
Andreas Pointner <andypoint@outlook.com>
2016-02-29 22:31:41 +0100

    restored normal main menu button configuration

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -152,12 +152,12 @@ public class MainMenu : MonoBehaviour
 	IEnumerator ScalePlayerImages() {
 
         //event build 
-        //GameObject[] buttonsNew = new GameObject[3];
+        GameObject[] buttonsNew = new GameObject[3];
 
-        GameObject[] buttonsNew = new GameObject[1];
+        //GameObject[] buttonsNew = new GameObject[1];
 		buttonsNew[0] = GameObject.Find("Crush_Poly_Text");
-		//buttonsNew[1] = GameObject.Find("Game_Stats_Text");
-		//buttonsNew[2] = GameObject.Find("Leave_Game_Text");
+        buttonsNew[1] = GameObject.Find("Game_Stats_Text");
+        buttonsNew[2] = GameObject.Find("Leave_Game_Text");
 
         Text[] texts = GameObject.FindObjectsOfType<Text>();
 
