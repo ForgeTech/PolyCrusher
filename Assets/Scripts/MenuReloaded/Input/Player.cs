e51725328d1ee2ee08f78fe4a8cba359fef94a53
Andreas Pointner <andypoint@outlook.com>
2016-08-15 13:04:55 +0200

    added player dummy script

---
+++ Assets/Scripts/MenuReloaded/Input/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public PlayerControlActions Actions { get; set; }
+
+    Renderer cachedRenderer;
+
+
+    void OnDisable()
+    {
+        if (Actions != null)
+        {
+            Actions.Destroy();
+        }
+    }
+
+
+    void Start()
+    {
+        cachedRenderer = GetComponent<Renderer>();
+    }
+
+
+    void Update()
+    {
+        if (Actions == null)
+        {
+            // If no controller exists for this cube, just make it translucent.
+            cachedRenderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
+        }
+        else
+        {
+            // Set object material color.
+            cachedRenderer.material.color = GetColorFromInput();
+
+            // Rotate target object.
+            transform.Rotate(Vector3.down, 500.0f * Time.deltaTime * Actions.Back, Space.World);
+            transform.Rotate(Vector3.right, 500.0f * Time.deltaTime * Actions.LeftHorizontal, Space.World);
+
+
+        }
+    }
+
+
+    Color GetColorFromInput()
+    {
+        if (Actions.Join)
+        {
+            return Color.green;
+        }
+
+        if (Actions.Back)
+        {
+            return Color.red;
+        }
+
+        if (Actions.Pause)
+        {
+            return Color.blue;
+        }
+
+        if (Actions.Ability)
+        {
+            return Color.yellow;
+        }
+
+        return Color.white;
+    }
+}
+
