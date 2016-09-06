1f4d7a7e61a33ee7f10f9291920bb6e07e0f6eca
Parzival Legmaier <legmaier@gmx.at>
2016-09-06 10:48:50 +0200

    Added some leaderboard request selection stuff. (Only for preparation of the real steam request)

---
+++ Assets/Scripts/MenuReloaded/ActionHandler/LeaderboardApplyAction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class LeaderboardApplyAction : AbstractActionHandler
+{
+    private AbstractMenuManager menuManager;
+    private SelectorWithSubSelector selector;
+
+    private void Start ()
+    {
+        menuManager = transform.parent.gameObject.GetComponent<AbstractMenuManager>();
+        selector = (SelectorWithSubSelector)menuManager.Selector;
+    }
+
+    public override void PerformAction<T> (T triggerInstance)
+    {
+        InvokeSteamRequest();
+        OnActionPerformed();
+    }
+
+    public void InvokeSteamRequest ()
+    {
+        GetInvocationData();
+    }
+
+    public void GetInvocationData()
+    {
+        // Iterate through all selector components (Levels, Character count)
+        foreach (var pair in selector.SubSelectionEntries)
+        {
+            // The apply button has no components
+            if (pair.Value.Components.Count > 0)
+            {
+                GameObject selectedMenuComponent = pair.Value.Components[pair.Value.Current];
+                AbstractActionHandler componentAction = selectedMenuComponent.GetComponent<AbstractActionHandler>();
+
+                // Apply the action of each menu component
+                componentAction.PerformAction<AbstractActionHandler>(this);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MenuReloaded/ActionHandler/LeaderboardMenuActions/LeaderboardWorldAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public enum LeaderboardWorld
+{
+    AllWorlds = 0,
+    Skyrena = 1,
+    BurningMan = 2,
+    GreenMamba = 3,
+    TrickOrTreat = 4,
+    IceAndSpice = 5,
+    SandMan = 6,
+    AtomicPond = 7
+}
+
+public class LeaderboardWorldAction : AbstractActionHandler
+{
+    [SerializeField]
+    public LeaderboardWorld world = LeaderboardWorld.AllWorlds;
+
+    public override void PerformAction<T>(T triggerInstance)
+    {
+        OnActionPerformed();
+    }
+}
