9076a31408be6bf8616c26d97e34b99e30c4dd87
Parzival Legmaier <legmaier@gmx.at>
2016-09-07 17:59:28 +0200

    New Random name generator method DJ snowface spawns in wave 3 MenuScore text update

--- Assets/Scripts/DataMining/RandomNameGenerator.cs
+++ Assets/Scripts/DataMining/RandomNameGenerator.cs
@@ -1,50 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+using UnityEngine;
 
 class RandomNameGenerator
 {
-    static readonly List<string> names = new List<string>();
-    static readonly Random rnd = new Random();
+    static string[] names;
+    static readonly System.Random rnd = new System.Random();
+    private static readonly char[] seperator = new char[] { '\r', '\n' };
 
     public static string Generate()
     {
-        // if names have not been loaded, load them
-        if(names.Count <= 0)
+        try
         {
-            try { 
-                StreamReader theReader = new StreamReader("Assets/Resources/names.txt", Encoding.Default);
-                string line = theReader.ReadLine();
-
-                using (theReader)
-                {
-                    if (line != null)
-                    {
-                        do
-                        {
-                            line = theReader.ReadLine();
-
-                            if(line != "")
-                            {
-                                names.Add(line);
-                            }
-
-                            line = theReader.ReadLine();
-                        }
-                        while (line != null);
-                    }
-
-                    theReader.Close();
-                }
-            }catch(Exception e)
+            if (names == null)
             {
-                names.Add("NONAME");
+                TextAsset textAsset = Resources.Load<TextAsset>("names");
+                if (textAsset != null)
+                    names = textAsset.text.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
             }
         }
-        
-        int location = rnd.Next(names.Count - 1);
+        catch {
+            return "NONAME";
+        }
 
+        int location = rnd.Next(names.Length - 1);
         return names[location].ToUpper();
     }
 }
