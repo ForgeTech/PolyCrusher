4d0726fe9a1b42f5f65d25fe66bdbf94bac8c299
Parzival Legmaier <legmaier@gmx.at>
2016-09-08 09:31:26 +0200

    Added DataCollector reset to the pause quit action.

--- Assets/Scripts/MenuReloaded/ActionHandler/PauseQuitAction.cs
+++ Assets/Scripts/MenuReloaded/ActionHandler/PauseQuitAction.cs
@@ -1,12 +1,9 @@
 ﻿public class PauseQuitAction : LevelLoadByNameWithAudioActivation
 {
-    public override void PerformAction<T>(T triggerInstance)
+    public override void PerformAction<T> (T triggerInstance)
     {
-        // TODO: Implement reset method for DataCollector in order to get the pause menu quit to work.
-        // DataCollector.instance.endSession();
-        
         // TODO: Call steam manager reset method
-
+        DataCollector.instance.Reset();
         base.PerformAction<T>(triggerInstance);
     }
 }
