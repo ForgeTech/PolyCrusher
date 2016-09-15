ece128783f5d0bb365a3b68d6447d379174125c4
Andreas Pointner <andypoint@outlook.com>
2016-09-15 16:33:51 +0200

    -smartphonecontroller script adjusted -controller manager now implements the virtualcontrollerhandler interface -set the enemyhealthincreasefactor according to the balancing data

--- Assets/Scripts/MenuReloaded/Input/ControllerManager.cs
+++ Assets/Scripts/MenuReloaded/Input/ControllerManager.cs
@@ -1,43 +1,117 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using InControl;
+using System;
 
-public class ControllerManager : MonoBehaviour
+public class ControllerManager : MonoBehaviour, VirtualControllerHandler
 {
-    //private List<SmartphoneController> smartphoneControllerList;
-    //private SmartphoneController smartphoneController;
+
+    #region variables
+    private SmartphoneController smartphoneController;
     private KeyboardController keyboardController;
 
+    private Dictionary<VirtualController, SmartphoneController> smartphoneControllers;
 
-   
-    void OnEnable()
-    {
-        //smartphoneControllerList = new List<SmartphoneController>();        
-    }
+    private int currentSmartPhoneController = 0;
+    private int maxSmartphoneConroller = 4;
+    #endregion
 
+    #region methods
     void Start()
     {
-        //-------------for testing purposes, needs to be deleted later on
-        //smartphoneController = new SmartphoneController(0, networkController);
-        //InputManager.AttachDevice(smartphoneController);
-        //smartphoneControllerList.Add(smartphoneController);
-        //-------------  
-
+        smartphoneControllers = new Dictionary<VirtualController, SmartphoneController>(4);
         keyboardController = new KeyboardController();
         InputManager.AttachDevice(keyboardController);
     }
 
-
-    void OnDisable()
+    void OnDestroy()
     {
-        //InputManager.DetachDevice(smartphoneController);
         InputManager.DetachDevice(keyboardController);
-    }
-   
-    void OnSmartphoneJoin(int smartphoneID)
-    {
-        //smartphoneController = new SmartphoneController(smartphoneID, networkController);
+        foreach (KeyValuePair<VirtualController, SmartphoneController> entry in smartphoneControllers)
+        {
+            InputManager.DetachDevice(entry.Value);
+        }
     }
 
-    
+    #region controller updates
+    public void VirtualControllerMoves(VirtualController virtualController, Vector2 movement)
+    {
+        smartphoneControllers.TryGetValue(virtualController, out smartphoneController);
+        if (smartphoneController != null)
+        {
+            smartphoneController.SetLeftAnalogStick = movement; 
+        }
+    }
+
+    public void VirtualControllerShoots(VirtualController virtualController, Vector2 shoot)
+    {
+        smartphoneControllers.TryGetValue(virtualController, out smartphoneController);
+        if (smartphoneController != null)
+        {
+            smartphoneController.SetRightAnalogStick = shoot;
+        }
+    }
+
+    public void VirtualControllerSendsSpecialAttack(VirtualController virtualController)
+    {
+        smartphoneControllers.TryGetValue(virtualController, out smartphoneController);
+        if (smartphoneController != null)
+        {
+            smartphoneController.SetAbilityPressed = true;
+        }
+    }
+
+    public void VirtualControllerSendsBackButton(VirtualController virtualController)
+    {
+        smartphoneControllers.TryGetValue(virtualController, out smartphoneController);
+        if (smartphoneController != null)
+        {
+            smartphoneController.SetBackPressed = true;
+        }
+    }
+
+    public void VirtualControllerSendsPauseButton(VirtualController virtualController)
+    {
+        smartphoneControllers.TryGetValue(virtualController, out smartphoneController);
+        if (smartphoneController != null)
+        {
+            smartphoneController.SetPausePressed = true;
+        }
+    }
+
+    public void VirtualControllerQuitsTheGame(VirtualController virtualController)
+    {
+        RemoveVirtualController(virtualController);
+    }
+
+    public void VirtualControllerIsNotResponsing(VirtualController virtualController)
+    {
+        Debug.Log("Controller is not responding!");
+    }
+    #endregion
+
+    #region managing smartphone controllers
+    public bool AddNewVirtualController(VirtualController virtualController)
+    {
+        if(currentSmartPhoneController < maxSmartphoneConroller)
+        {
+            currentSmartPhoneController++;
+            smartphoneController = new SmartphoneController(virtualController);
+            smartphoneControllers.Add(virtualController, smartphoneController);
+        }
+        return false;
+    }
+
+    private void RemoveVirtualController(VirtualController virtualController)
+    {
+        smartphoneControllers.TryGetValue(virtualController, out smartphoneController);
+        if (smartphoneController != null)
+        {
+            InputManager.DetachDevice(smartphoneController);
+            currentSmartPhoneController--;
+        }
+    }
+    #endregion
+
+    #endregion
 }
