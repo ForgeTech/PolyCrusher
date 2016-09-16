3c39d43b8f1d8d9765fc330bfafa446d7f3b44f0
Parzival Legmaier <legmaier@gmx.at>
2016-09-16 12:34:50 +0200

    Added  - Basic arrows (currently no reactions to damage, etc.)
    Fixed Burning man occlusion bug with pantomime.

---
+++ Assets/Scripts/Player/PlayerArrow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerArrow : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 30f;
+
+    private float currentYRotation = 0f;
+    private Quaternion arrowRotation;
+    private Quaternion originalRotation;
+
+    private void Start()
+    {
+        originalRotation = transform.rotation;
+        arrowRotation = originalRotation;
+    }
+
+	private void Update ()
+    {
+        ResetParentRotation();
+        RotateYAxis();
+	}
+
+    private void ResetParentRotation()
+    {
+        transform.rotation = originalRotation;
+    }
+
+    private void RotateYAxis()
+    {
+        //currentYRotation += Time.deltaTime * rotationSpeed;
+        //transform.Rotate(new Vector3(-90, currentYRotation, 0));
+
+        arrowRotation *= Quaternion.Euler(Vector3.up * Time.deltaTime * rotationSpeed);
+        transform.rotation = arrowRotation;
+    }
+}
