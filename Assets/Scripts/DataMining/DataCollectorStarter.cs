f32a3f25a79f38be5e64343fdf0e56a3496cca08
Parzival Legmaier <legmaier@gmx.at>
2016-10-04 16:15:33 +0200

    Added functionality for a commandline event build start. (Currently it looks like the command line param is ignored)

--- Assets/Scripts/DataMining/DataCollectorStarter.cs
+++ Assets/Scripts/DataMining/DataCollectorStarter.cs
@@ -9,9 +9,25 @@ public class DataCollectorStarter : MonoBehaviour
     [SerializeField]
     private DataCollectorSettings settings;
 
+    private static bool commandLineEventBuild = false;
+
 	private void Start ()
     {
+        // Set event build based on command line Flag
+        if (commandLineEventBuild && settings != null)
+            settings.eventBuild = true;
+
         if(FindObjectOfType<DataCollector>() == null)
             DataCollector.Initialize(settings);
 	}
+
+    /// <summary>
+    /// Static method which should be called per command line argument
+    /// Example: -executeMethod DataCollectorStarter.StartEventBuild
+    /// </summary>
+    static void StartEventBuild()
+    {
+        Debug.Log("Event Build Activated per command line argument.");
+        commandLineEventBuild = true;
+    }
 }
